eb2109e05123c83d10ed48a1e4782e1a7c5a5196
Huỳnh Đức <hiepthaitran2001@gmail.com>
2023-05-07 13:21:57 +0700

    update

---
+++ src/dto/HoaDon.java
@@ -0,0 +1,17 @@
+
+package dto;
+
+import java.util.Date;
+
+
+public class HoaDon {
+    private int MaHD;
+    private Date NgayLap;
+
+    public HoaDon(int MaHD, Date NgayLap) {
+        this.MaHD = MaHD;
+        this.NgayLap = NgayLap;
+    }
+    
+    
+}
